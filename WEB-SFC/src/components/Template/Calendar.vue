4cb7a4be742db57fa9b23bf6fcd482d763501e72
unknown <V1039442@VNGZ.FOXCONN.COM>
2024-09-18 08:12:21 +0700

    delete websfc

--- WEB-SFC/src/components/Template/Calendar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <datepicker v-model="picked" />
-</template>
-<script setup>
-
-import { ref } from 'vue'
-const picked = ref(new Date())
-</script>
--- WEB-SFC/src/vuetify.js
+++
@@ -1,7 +0,0 @@
-import Vuetify from 'vuetify'
-import Vue from 'vue'
-import 'vuetify/dist/vuetify.min.css'
-
-Vue.use(Vuetify)
-
-export default new Vuetify({})
