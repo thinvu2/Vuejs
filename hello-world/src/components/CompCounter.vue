bdd71e003a395abd7540db0dfcde57b0005cf293
thinvu2 <thinfaker20@gamil.com>
2024-10-07 17:29:44 +0700

    a

--- hello-world/src/components/CompCounter.vue
+++ hello-world/src/components/CompCounter.vue
@@ -1,23 +1,28 @@
 <template>
     <div>
         <h1>{{ count }}</h1>
-        <button v-on:click="handleCountPlus">Plus</button>
-        <button v-on:click="handleCountMinus">Minus</button>
+        <button v-on:click="count++">Plus</button>
+        <button v-on:click="count--">Minus</button>
     </div>
 </template>
 <script>
 export default {
     name: 'comp-counter',
-    props:{
-            count: {
-                default: 0,
-                type: Number
-            }
-        },
+    // props:{
+    //         count: {
+    //             default: 0,
+    //             type: Number
+    //         }
+    //     },
     data() {
         return{
         }
     },
+    computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
     methods: {
         handleCountPlus() {
             this.$emit('handleCountPlus');
--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,21 +1,7 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
+import store from './store'
 import App from './App.vue'
 
-const store = createStore({
-    state () {
-      return {
-        count: 0
-      }
-    },
-    mutations: {
-      increment (state) {
-        state.count++
-      }
-    }
-  });
-  const app = createApp({ /* your root component */ })
-
-  // Install the store instance as a plugin
-  app.use(store)
- createApp(App).mount('#app')
+const app = createApp(App);
+app.use(store)
+app.mount('#app')
